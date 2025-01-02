fff68b064c0fa2f3b57ceed8c5f9be6bb6d68732
Dipak Gupta <dipakg286@gmail.com>
2025-01-02 21:02:51 +0530

    First commit

---
+++ src/main/java/com/atom/search/AtomSearchServiceApplication.java
@@ -0,0 +1,11 @@
+package com.atom.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomSearchServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AtomSearchServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/atom/search/AtomSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atom.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
