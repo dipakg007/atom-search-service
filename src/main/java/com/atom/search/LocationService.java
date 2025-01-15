de5de7d3c08a0e4c7e7795a6ddd1ae7efbfb6dc8
Dipak Gupta <dipakg286@gmail.com>
2025-01-15 18:04:26 +0530

    spotless apply plugins added for formatting

--- src/main/java/com/atom/search/LocationService.java
+++ src/main/java/com/atom/search/LocationService.java
@@ -1,10 +1,10 @@
 package com.atom.search;
 
+import static com.atom.utils.LocationUtils.calculateDistance;
+
 import com.atom.dto.LocationDTO;
 import com.atom.entities.Location;
 import com.atom.repository.LocationRepository;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.locationtech.jts.geom.Point;
@@ -43,26 +43,5 @@ public class LocationService {
     }
 
     return dto;
-  }
-
-  public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-    final int R = 6371;
-
-    double latDistance = Math.toRadians(lat2 - lat1);
-    double lonDistance = Math.toRadians(lon2 - lon1);
-
-    double a =
-        Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-            + Math.cos(Math.toRadians(lat1))
-                * Math.cos(Math.toRadians(lat2))
-                * Math.sin(lonDistance / 2)
-                * Math.sin(lonDistance / 2);
-    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    double distance = R * c;
-
-    BigDecimal bd = new BigDecimal(distance).setScale(4, RoundingMode.HALF_UP);
-
-    return bd.doubleValue();
   }
 }
---
+++ src/main/java/com/atom/utils/LocationUtils.java
@@ -0,0 +1,27 @@
+package com.atom.utils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class LocationUtils {
+  public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    final int R = 6371;
+
+    double latDistance = Math.toRadians(lat2 - lat1);
+    double lonDistance = Math.toRadians(lon2 - lon1);
+
+    double a =
+        Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+            + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2)
+                * Math.sin(lonDistance / 2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    double distance = R * c;
+
+    BigDecimal bd = new BigDecimal(distance).setScale(4, RoundingMode.HALF_UP);
+
+    return bd.doubleValue();
+  }
+}
